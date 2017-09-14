ed2210087f21d8740aa79c99247375f564b27a15
Yuriy Tarnavskiy <yuriy.g.tarnavskiy@gmail.com>
2017-09-14 15:29:07 +0300

    initial commit

---
+++ pet1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pet1
+//
+//  Created by Yuriy T on 14.09.17.
+//  Copyright © 2017 xeon.pet1.education. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
